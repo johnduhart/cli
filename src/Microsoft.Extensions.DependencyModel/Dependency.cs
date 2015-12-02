a2c1dbd295400577c1ea7d55d6355ab9fbdb9432
Pavel Krymets <pavel@krymets.com>
2015-12-02 13:56:35 -0800

    add classes

---
+++ src/Microsoft.Extensions.DependencyModel/Dependency.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Extensions.DependencyModel
+{
+    public class Dependency
+    {
+        public Dependency(string assemblyName, string packageName, string version, string checksum)
+        {
+            AssemblyName = assemblyName;
+            PackageName = packageName;
+            Version = version;
+            Checksum = checksum;
+        }
+
+        public string AssemblyName { get; }
+
+        public string PackageName { get; }
+
+        public string Version { get; }
+
+        public string Checksum { get; }
+    }
+}
---
+++ src/Microsoft.Extensions.DependencyModel/DependencyContext.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+
+namespace Microsoft.Extensions.DependencyModel
+{
+    public class DependencyContext
+    {
+        private const string DepsExtension = ".deps";
+
+        public DependencyContext()
+        {
+        }
+
+        public DependencyContext(IList<Dependency> dependencies)
+        {
+            Dependencies = dependencies;
+        }
+
+        public IList<Dependency> Dependencies { get; }
+
+        public static DependencyContext Load()
+        {
+            Assembly entryAssembly = null;
+            using (var fileStream = File.OpenRead(entryAssembly.Location))
+            {
+                return Load(fileStream);
+            }
+        }
+
+        private static DependencyContext Load(Stream stream)
+        {
+            return new DependencyContext();
+        }
+    }
+}
